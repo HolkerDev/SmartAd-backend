e2b22fb2f2dce3a564d6c799900fd85dc9a7b6c5
Mykola Penkovskyi <36474981+HolkerDev@users.noreply.github.com>
2022-09-08 09:13:29 +0200

    add handler tests

---
+++ src/tests/test_register_handler.py
@@ -0,0 +1,29 @@
+import json
+import unittest
+from unittest import mock
+
+from src.functions.register.handler import register
+from src.utils.aws_request import AwsRequest
+from src.utils.models import User
+
+
+class RegisterHandlerModuleTest(unittest.TestCase):
+    @mock.patch("src.functions.register.handler.user_repository")
+    def test_new_user_registration(self, repo):
+        request_body = json.dumps({"email": "test@email.com", "password": "my_pass"})
+        repo.find_by_email.return_value = None
+        repo.create.return_value = None
+        response = register(AwsRequest({"body": request_body}))
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch("src.functions.register.handler.user_repository")
+    def test_new_user_email_exists(self, repo):
+        request_body = json.dumps({"email": "test@email.com", "password": "my_pass"})
+        repo.find_by_email.return_value = User("test@email.com")
+        repo.create.return_value = None
+        response = register(AwsRequest({"body": request_body}))
+        self.assertEqual(response.status_code, 409)
+
+
+if __name__ == "__main__":
+    unittest.main()
